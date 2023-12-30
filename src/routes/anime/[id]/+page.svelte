b70b3a3a6db4d9cb117618fea7e7f019132f7c27
Tou-u <rodrignacio99@hotmail.com>
2023-12-30 20:58:01 -0300

    bg blur for animes

--- src/routes/anime/[id]/+page.svelte
+++ src/routes/anime/[id]/+page.svelte
@@ -4,17 +4,14 @@
 
   export let data: PageData
   const { anime } = data
+
+  const image = `${baseUrl}${anime.image.original}`
 </script>
 
-<main class="min-h-screen bg-black">
-  <header class="h-60 blur-md">
-    <img
-      alt="header"
-      src="{baseUrl}{anime.image.original}"
-      class="h-full w-full object-cover object-center"
-    />
-  </header>
-
+<div class="overflow-hidden absolute top-0 left-0 right-0 bottom-0 -z-10 brightness-50">
+  <img src={image} class="w-full h-full blur-3xl scale-[4] object-cover" alt="background" />
+</div>
+<main class="min-h-screen p-4">
   <section class="px-4 max-w-screen-xl mx-auto">
     <article class="flex flex-col items-center justify-center gap-5 sm:flex-row">
       <img
@@ -22,7 +19,7 @@
         alt={anime.name}
         src="{baseUrl}{anime.image.original}"
         style="view-transition-name: img-{anime.id};"
-        class="-mt-52"
+        class="h-96"
       />
       <h1 class="text-2xl font-semibold text-white text-center">{anime.name}</h1>
     </article>
